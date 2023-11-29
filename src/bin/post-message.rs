607cb7f46c6f2404db36da0980b61bc62c048044
Bianca Pelegrini <bmpelegrini@gmail.com>
2023-11-29 15:03:21 -0300

    successfully connects and executes command

--- src/bin/post-message.rs
+++ src/bin/post-message.rs
@@ -4,7 +4,7 @@ fn main() {
     let tokio_rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
-        .expect("failed to initialize tokio runtime");
+        .expect("Failed to initialize tokio runtime");
 
     let future = tokio_rt.block_on(async move { main_async().await });
 
@@ -19,7 +19,20 @@ async fn main_async() -> anyhow::Result<(), Error> {
     let redis_conn_str =
         std::env::var("REDIS_CONNECTION_STRING").expect("Missing REDIS_CONNECTION_STRING");
 
-    println!("REDIS_CONNECTION_STRING {}", redis_conn_str);
+    println!("Running with REDIS_CONNECTION_STRING {}", redis_conn_str);
+
+    let client = redis::Client::open(redis_conn_str).expect("Failed to open redis client");
+    let mut connection = client
+        .get_multiplexed_tokio_connection()
+        .await
+        .expect("Failed to get redis connection");
+
+    let result: String = redis::cmd("PING")
+        .query_async(&mut connection)
+        .await
+        .expect("Failed to ping redis server");
+
+    println!("{} should be PONG", result);
 
     Ok(())
 }
